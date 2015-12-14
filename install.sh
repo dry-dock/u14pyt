1ce20db4faf0700252da81afbb448f17f1b74ff3
chandrakanth <chandrakanth@shippable.com>
2015-12-14 03:00:07 -0500

    changed tmp to u14pyt in Dockerfile~~~

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash -e
 
+sudo apt-get clean
+sudo mv /var/lib/apt/lists /tmp
+sudo mkdir -p /var/lib/apt/lists/partial
+sudo apt-get clean
+sudo apt-get update
+
 # install python prereqs
 add-apt-repository -y ppa:fkrull/deadsnakes
 apt-get update
 apt-get install -y libxml2 libxml2-dev libxslt1.1 libxslt1-dev libffi-dev libssl-dev
 
 #!/bin/bash
-for file in /tmp/version/*;
+for file in /u14pyt/version/*;
 do
   $file
 done
