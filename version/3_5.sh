fdb7d0b31cd92dca69244e9dd99c55c42aab8bcf
chandrakanth <chandrakanth@shippable.com>
2015-12-14 06:12:07 -0500

    changed folder name form python3.5 to 3.5

--- version/3_5.sh
+++ version/3_5.sh
@@ -5,9 +5,9 @@ wget https://bootstrap.pypa.io/get-pip.py
 python3.5 get-pip.py
 pip install virtualenv
 
-virtualenv -p python3.5 $HOME/venv/python3.5
+virtualenv -p python3.5 $HOME/venv/3.5
 # Install pip packages
-. $HOME/venv/python3.5/bin/activate
+. $HOME/venv/3.5/bin/activate
 pip install --upgrade wheel
 pip install nose mock pytest coverage
 deactivate
